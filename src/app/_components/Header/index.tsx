44cf71d377d5cf2a75ca0c1be199d1e91fd30fde
LoreisLearnCode <loreis.learncode@gmail.com>
2024-06-25 12:19:06 +0700

    update

---
+++ src/app/_components/Header/index.tsx
@@ -0,0 +1,29 @@
+{
+  /* eslint-disable @next/next/no-img-element */
+}
+
+import React from 'react'
+import Link from 'next/link'
+
+import { Header } from '../../../payload/payload-types'
+import { fetchHeader } from '../../_api/fetchGlobals'
+// import { Gutter } from '../Gutter'
+// import { HeaderNav } from './Nav'
+import HeaderComponent from './HeaderComponent'
+// import classes from './index.module.scss'
+
+export async function Header() {
+  let header: Header | null = null
+
+  try {
+    header = await fetchHeader()
+  } catch (error) {
+    console.log(error)
+  }
+
+  return (
+    <>
+      <HeaderComponent header={header} />
+    </>
+  )
+}
